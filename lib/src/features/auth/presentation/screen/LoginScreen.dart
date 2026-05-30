72d9ddb2c83e1781389632c903865cec455cb00d
Moimen Adel <moimenadel156@gmail.com>
2026-05-30 16:19:42 +0300

    feat: Add some of  LoginScreen

--- lib/src/features/auth/presentation/screen/LoginScreen.dart
+++ lib/src/features/auth/presentation/screen/LoginScreen.dart
@@ -1,15 +1,46 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:shop_z/src/extensions/spAdabt.dart';
+import 'package:shop_z/src/imports/core_imports.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login Screen'),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Login to your account',
+              style: context.textTheme.headlineMedium!.copyWith(
+                fontWeight: FontWeight.bold,
+                color: context.colors.onSurface,
+              ),
+            ),
+            10.kH,
+            Text(
+              'It’s great to see you again.',
+              style: context.textTheme.bodyMedium!.copyWith(
+                color: context.colors.secondary,
+              ),
+            ),
+            24.kH,
+            Text(
+              'User Name',
+              style: context.textTheme.titleMedium!.copyWith(
+                color: context.colors.onSurface,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            4.kH,
+          ],
+        ).paddingSymmetric(
+            vertical: context.designTokens.paddingMedium.spAdaptive(context),
+            horizontal: context.designTokens.paddingLarge.spAdaptive(context)),
       ),
     );
   }
