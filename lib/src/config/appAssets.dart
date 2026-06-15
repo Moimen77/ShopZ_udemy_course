bf8de8d4438d6cfc9683aa7e3cf7d574113bb733
Moimen Adel <moimenadel156@gmail.com>
2026-06-15 14:30:36 +0300

    Add Loading Lottie in App And added it in the file Assets

---
+++ lib/src/config/appAssets.dart
@@ -0,0 +1,8 @@
+class Appassets {
+  static String images = 'assets/images';
+  static String icons = 'assets/icons';
+  static String lottie = 'assets/Lottie';
+
+  //Lottie
+  static String loading = '$lottie/loading.json';
+}
